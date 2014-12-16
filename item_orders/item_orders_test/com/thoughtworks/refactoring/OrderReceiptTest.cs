9cf8fb0d7c644a92ba61647c4e9a72940bb708eb
Anubhav Srivastava <anubhav.v.sri@gmail.com>
2014-12-16 10:43:31 +0530

    fixed test for item_orders(removed unnecessary decimals

--- item_orders/item_orders_test/com/thoughtworks/refactoring/OrderReceiptTest.cs
+++ item_orders/item_orders_test/com/thoughtworks/refactoring/OrderReceiptTest.cs
@@ -22,17 +22,17 @@ public class OrderReceiptTest {
     [Test]
     public void ShouldPrintLineItemAndSalesTaxInformation() {
         List<LineItem> lineItems = new List<LineItem>() {
-            new LineItem("milk", 10.0, 2),
-            new LineItem("biscuits", 5.0, 5),
-            new LineItem("chocolate", 20.0, 1)
+				new LineItem("milk", 10.0d, 2),
+				new LineItem("biscuits", 5.0d, 5),
+				new LineItem("chocolate", 20.0d, 1)
         };
         OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
 
         String output = receipt.printReceipt();
 
-        Assert.That(output, Contains.Substring("milk\t10.0\t2\t20.0\n"));
-        Assert.That(output, Contains.Substring("biscuits\t5.0\t5\t25.0\n"));
-        Assert.That(output, Contains.Substring("chocolate\t20.0\t1\t20.0\n"));
+        Assert.That(output, Contains.Substring("milk\t10\t2\t20\n"));
+        Assert.That(output, Contains.Substring("biscuits\t5\t5\t25\n"));
+        Assert.That(output, Contains.Substring("chocolate\t20\t1\t20\n"));
         Assert.That(output, Contains.Substring("Sales Tax\t6.5"));
         Assert.That(output, Contains.Substring("Total Amount\t71.5"));
     }
